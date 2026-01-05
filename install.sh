7c1c83be0357db5e736161f06052556f399bd591
Kaushal Choudhary <kaushalc64@gmail.com>
2026-01-05 17:33:55 +0000

    update_installer

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
 set -e
 
-REPO="kaushal07wick/osmogrep"
+REPO="kaushal07wick/OsmoGrep"
 VERSION="v0.1.0"
 BIN="osmogrep-linux-x86_64"
 INSTALL_DIR="/usr/local/bin"
 
 echo "Installing osmogrep..."
 
+tmp="$(mktemp)"
+
 curl -fsSL \
   "https://github.com/$REPO/releases/download/$VERSION/$BIN" \
-  -o osmogrep
+  -o "$tmp"
 
-chmod +x osmogrep
+chmod +x "$tmp"
 
 if [ -w "$INSTALL_DIR" ]; then
-  mv osmogrep "$INSTALL_DIR/osmogrep"
+  mv "$tmp" "$INSTALL_DIR/osmogrep"
 else
-  sudo mv osmogrep "$INSTALL_DIR/osmogrep"
+  sudo mv "$tmp" "$INSTALL_DIR/osmogrep"
 fi
 
 echo "osmogrep installed successfully"
